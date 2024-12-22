f31680f3a8c9ae9930ccc94b8508dcd54648cf4a
db-0 <dan@dbsphere.net>
2024-12-21 23:37:10 -0700

    added formatting and individual character counts

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_count = get_char_count(text)
-    print(f"There are {word_count} words in {book_path}")
-    print(f"The character counts for each letter are:\n{char_count}")
+    char_count.sort(reverse=True, key=sort_on)
+    print_report(book_path, word_count, char_count)
+
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def get_book_text(path):
@@ -17,13 +21,25 @@ def get_word_count(text):
 
 
 def get_char_count(text):
-    characters = {}
-    for char in text.lower():
-        if not char in characters:
-            characters[char] = 1
-        else:
-            characters[char] += 1
-    return characters
+    chars = {}
+    char_lod = []
+    for c in text.lower():
+        if c.isalpha():
+            if not c in chars:
+                chars[c] = 1
+            else:
+                chars[c] += 1
+    for k,v in chars.items():
+        char_lod.append({"char": k, "num": v})
+    return char_lod
+
+
+def print_report(path, words, chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"There are {words} words in this document.\n")
+    for dict in chars:
+        print(f"The '{dict["char"]}' character was found {dict["num"]} times")
+    print("--- End Report ---")
 
 
 main()
